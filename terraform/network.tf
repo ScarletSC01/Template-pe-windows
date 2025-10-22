8736a61fcc1078b273e8cac1c7ce2ba3da8a61ba
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-10-22 16:04:06 -0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,7 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = var.VPC_NETWORK
+  name                    = "vpc-pe-windows"
   auto_create_subnetworks = false
 }
-
 resource "google_compute_subnetwork" "subnet" {
   name          = var.SUBNET
   ip_cidr_range = var.NETWORK_SEGMENT
