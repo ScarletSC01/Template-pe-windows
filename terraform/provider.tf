6e9cbf1ce9afe6bce8104f33165fabbba98fd339
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-10-22 12:48:45 -0300

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.PROJECT_ID
+  region  = var.REGION
+  zone    = var.ZONE
+}
